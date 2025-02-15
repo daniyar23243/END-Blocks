5351f2f4ae54dbc3c22dfa051756e98622ac5df7
Daniyar <donik12rrr@gmail.com>
2025-02-15 21:18:35 +0600

    add

---
+++ script.js
@@ -0,0 +1,17 @@
+function allowDrop (ev) {
+  ev.preventDefault();
+}
+
+function drag (ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop (ev) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  document.getElementById(data).addEventListener("dragend", () => {
+    document.getElementById(data).draggable= false;  
+  })
+}
+
