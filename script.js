c30231f7cd3a9a5bc735e32efcc2b1695c3989be
Daniyar <donik12rrr@gmail.com>
2025-02-16 17:22:43 +0600

    we are getting somewhere

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ function drag (ev) {
 }
 
 function drop (ev) {
-  ev.preventDefault();
-  const data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-  document.getElementById(data).addEventListener("dragend", () => {
-    document.getElementById(data).draggable= false;  
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    const droppedElement = document.getElementById(data);
+    droppedElement.addEventListener("dragend", () => {
+      droppedElement.draggable= false;  
   })
+if (droppedElement.classList.contains("t-shape")) {
+  droppedElement.style.transform = "translateX(-3.25rem)";
+  }
+if (droppedElement.classList.contains("l-shape")) {
+    droppedElement.style.transform = "translateY(-3.25rem)";
+    }
+if (droppedElement.classList.contains("o-shape")) {
+    droppedElement.classList.add("moved");
+  }      
 }
+
+function dragStart(ev) {
+    console.log("dragging")
+    ev.target.classList.add("dragging");
+  }
+  
+  function dragEnd(ev) {
+    console.log("no drag")
+    ev.target.classList.remove("dragging");
+  }
 
